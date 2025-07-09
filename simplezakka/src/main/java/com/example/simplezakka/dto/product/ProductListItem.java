0881c9e3828fffbdc3496e912b8f8eec4a13a67a
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-09 14:43:53 +0900

    DTOクラスの実装

---
+++ simplezakka/src/main/java/com/example/simplezakka/dto/product/ProductListItem.java
@@ -0,0 +1,29 @@
+package com.example.simplezakka.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductListItem {
+    private Integer productId;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+}package com.example.simplezakka.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductListItem {
+    private Integer productId;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+}
