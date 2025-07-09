3ccb1f8f20544f61b5d0b301b4f5b0eb4ab5b74e
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-09 16:14:49 +0900

    ProductListItem.javaの修正

--- simplezakka/src/main/java/com/example/simplezakka/dto/product/ProductListItem.java
+++ simplezakka/src/main/java/com/example/simplezakka/dto/product/ProductListItem.java
@@ -12,18 +12,4 @@ public class ProductListItem {
     private String name;
     private Integer price;
     private String imageUrl;
-}package com.example.simplezakka.dto.product;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductListItem {
-    private Integer productId;
-    private String name;
-    private Integer price;
-    private String imageUrl;
 }
