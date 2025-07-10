753c0f80409579c56b29c72095ca7473e3126a14
DaisukeShimada402 <daisuke.shimada@avantcorp.com>
2025-07-10 13:22:25 +0900

    サンプルコード反映

--- raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderDetailRepository.java
+++ raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderDetailRepository.java
@@ -1,9 +1,9 @@
 package com.example.simplezakka.repository;
 
-import com.example.simplezakka.entity.Order;
+import com.example.simplezakka.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
 }
