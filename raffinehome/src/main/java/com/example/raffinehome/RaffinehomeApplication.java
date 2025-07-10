577b742d9f5d321afb71f2fd1f4b1f08f5553cba
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-10 10:50:23 +0900

    初期設定2

---
+++ raffinehome/src/main/java/com/example/raffinehome/RaffinehomeApplication.java
@@ -0,0 +1,14 @@
+package com.example.raffinehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffinehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffinehomeApplication.class, args);
+	}
+
+}
+       
