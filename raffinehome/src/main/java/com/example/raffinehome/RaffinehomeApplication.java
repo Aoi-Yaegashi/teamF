2f99b07a76944394ea876d5668bc301b592f8018
yuki-nishijima3 <yuki.nishijima@avantcorp.com>
2025-07-10 13:57:16 +0900

    raffinhomeアプリケーション作成

---
+++ raffinehome/src/main/java/com/example/raffinehome/RaffinehomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.raffinehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffinehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffinehomeApplication.class, args);
+	}
+
+}
