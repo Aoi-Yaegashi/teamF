7058a49798bf0aeaac0746563eb2a76ac697023e
yuki-nishijima3 <yuki.nishijima@avantcorp.com>
2025-07-10 16:56:58 +0900

    OrderCreateDTO編集

--- raffinehome/src/main/java/com/example/raffinehome/order/dto/OrderCreateDTO.java
+++ raffinehome/src/main/java/com/example/raffinehome/order/dto/OrderCreateDTO.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderCreate {
+public class OrderCreateDTO {
     private Integer orderId;
     private LocalDateTime orderDate;
 }
