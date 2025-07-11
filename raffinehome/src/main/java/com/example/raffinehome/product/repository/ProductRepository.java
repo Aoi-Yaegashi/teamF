ed2f370ebae960991e2d5c86d8e0450c9b9cb100
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-11 15:59:00 +0900

    repositoryの変更

--- raffinehome/src/main/java/com/example/raffinehome/product/repository/ProductRepository.java
+++ raffinehome/src/main/java/com/example/raffinehome/product/repository/ProductRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     
     @Modifying
-    @Query("UPDATE Product p SET p.stock_quantity = p.stock_quantity - ?2 WHERE p.id = ?1 AND p.stock_quantity >= ?2")
-    int decreaseStock(int id, int stock_quantity);
+    @Query("UPDATE Product p SET p.stockQuantity = p.stockQuantity - ?2 WHERE p.id = ?1 AND p.stockQuantity >= ?2")
+    int decreaseStock(int id, int stockQuantity);
 }
