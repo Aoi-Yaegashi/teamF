25bdd588e5b60fc7ed54889da34aef204d563f70
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-11 16:18:32 +0900

    entityの修正

--- raffinehome/src/main/java/com/example/raffinehome/order/entity/OrderDetail.java
+++ raffinehome/src/main/java/com/example/raffinehome/order/entity/OrderDetail.java
@@ -3,6 +3,9 @@ package com.example.raffinehome.order.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 import com.example.raffinehome.product.entity.Product;
 
 @Entity
@@ -13,7 +16,7 @@ public class OrderDetail {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer orderDetailId;
+    private int id;
     
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
@@ -27,8 +30,18 @@ public class OrderDetail {
     private String productName;
     
     @Column(nullable = false)
-    private Integer price;
+    private  int unitPrice;
     
     @Column(nullable = false)
-    private Integer quantity;
+    private int quantity;
+    
+    @Column(nullable = false)
+    private int subtotal;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
