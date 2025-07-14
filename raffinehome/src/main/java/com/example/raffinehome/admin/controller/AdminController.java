7ab3de8bbab6de59b74c6750ad4e96182c805595
DaisukeShimada402 <daisuke.shimada@avantcorp.com>
2025-07-14 16:32:49 +0900

    Update、Create作成

--- raffinehome/src/main/java/com/example/raffinehome/admin/controller/AdminController.java
+++ raffinehome/src/main/java/com/example/raffinehome/admin/controller/AdminController.java
@@ -1,3 +1,5 @@
+package com.example.raffinehome.admin.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
