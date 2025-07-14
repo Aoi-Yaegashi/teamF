d85b87a8d081da7d0affd22cbf1b6b9549cf136e
DaisukeShimada402 <daisuke.shimada@avantcorp.com>
2025-07-14 17:27:24 +0900

    エラー直し

--- raffinehome/src/main/java/com/example/raffinehome/product/dto/ProductCreateDTO.java
+++ raffinehome/src/main/java/com/example/raffinehome/product/dto/ProductCreateDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductDTO {
+public class ProductCreateDTO {
     private int id;
     private String name;
     private int price;
