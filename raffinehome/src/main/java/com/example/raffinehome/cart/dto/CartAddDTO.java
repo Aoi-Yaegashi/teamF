a34d8d27ca2411fb21f9f1a7f2dcf61afd0109f1
HinakoYoshida04 <hinako.yoshida@avantcorp.com>
2025-07-14 10:24:01 +0900

    名前変更

--- raffinehome/src/main/java/com/example/raffinehome/cart/dto/CartAddDTO.java
+++ raffinehome/src/main/java/com/example/raffinehome/cart/dto/CartAddDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class CartItemInfo {
+public class CartAddDTO {
     @NotNull(message = "商品IDは必須です")
     private Integer productId;
     
