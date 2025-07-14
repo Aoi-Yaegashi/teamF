525ae39f86509aa9b1ce53b24f9456a4683417fd
HinakoYoshida04 <hinako.yoshida@avantcorp.com>
2025-07-14 12:13:43 +0900

    あ

--- raffinehome/src/main/java/com/example/raffinehome/cart/dto/CartUpdateDTO.java
+++ raffinehome/src/main/java/com/example/raffinehome/cart/dto/CartUpdateDTO.java
@@ -8,11 +8,6 @@ import lombok.Data;
 public class CartUpdateDTO {
     @NotNull(message = "数量は必須です")
     @Min(value = 1, message = "数量は1以上である必要があります")
-<<<<<<< HEAD
-    private int quantity;
-    private int productId;
-=======
     private Integer quantity;
     private Integer productId;
->>>>>>> 739d32ff046cd5dce0f9b01bb67985579cc4a015
 }
