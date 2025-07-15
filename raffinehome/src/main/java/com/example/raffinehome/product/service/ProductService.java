b38c83057e513d39718d8bdc6bb37d95dfeaa552
DaisukeShimada402 <daisuke.shimada@avantcorp.com>
2025-07-15 13:34:13 +0900

    ああ

--- raffinehome/src/main/java/com/example/raffinehome/product/service/ProductService.java
+++ raffinehome/src/main/java/com/example/raffinehome/product/service/ProductService.java
@@ -42,9 +42,7 @@ public class ProductService {
             product.getDescription(),
             product.getStockQuantity(),
             product.getImageUrl(),
-            product.getStockQuantity() > 0,
-            
-            product.isDeleted()
+            product.getStockQuantity() > 0
         );
     }
     
