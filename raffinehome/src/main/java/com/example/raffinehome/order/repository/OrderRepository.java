7419654a0248302e5a343a2ab42d93616889eb99
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-18 13:24:36 +0900

    OrderRepository追加分削除

--- raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderRepository.java
+++ raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderRepository.java
@@ -12,11 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
-    List<Order> findByAdminUserIdOrderByOrderDateDesc(Integer adminUserId);
-
-    List<Order> findByOrderDateBetweenOrderByOrderDateAsc(LocalDateTime startDate, LocalDateTime endDate);
-
-    List<Order> findByOrderDateAfter(LocalDateTime dateTime);
-    
 }
 
