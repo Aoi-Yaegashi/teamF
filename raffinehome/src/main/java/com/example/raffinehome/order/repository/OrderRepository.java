a36e9c536214fb69b92c7ce129280e0915129075
^Aoi-Yaegashi <aoi.yaegashi@avantcorp.com>
2025-07-18 11:18:39 +0900

    Test用にコードの追加

--- raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderRepository.java
+++ raffinehome/src/main/java/com/example/raffinehome/order/repository/OrderRepository.java
@@ -1,9 +1,22 @@
 package com.example.raffinehome.order.repository;
 
 import com.example.raffinehome.order.entity.Order;
+
+import java.time.LocalDateTime;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    List<Order> findByAdminUserIdOrderByOrderDateDesc(Integer adminUserId);
+
+    List<Order> findByOrderDateBetweenOrderByOrderDateAsc(LocalDateTime startDate, LocalDateTime endDate);
+
+    List<Order> findByOrderDateAfter(LocalDateTime dateTime);
+    
 }
+
