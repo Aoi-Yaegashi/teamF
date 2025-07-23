63ac5509a7f28a72e8de84d6f9e3b52a6cee4d47
DaisukeShimada402 <daisuke.shimada@avantcorp.com>
2025-07-23 14:02:50 +0900

    バリデーション追加

--- raffinehome/src/main/java/com/example/raffinehome/admin/dto/AdminUpdateDTO.java
+++ raffinehome/src/main/java/com/example/raffinehome/admin/dto/AdminUpdateDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -16,15 +17,18 @@ public class AdminUpdateDTO{
     @NotBlank(message = "商品名は必須です")
     private String name;
 
-    
     private String description;
 
-    @NotBlank(message = "商品価格は必須です")
-    @Min(value = 1, message = "価格は1以上である必要があります")
+    @NotNull(message = "商品価格は必須です")
+    @Min(value = 1, message = "価格は1以上で入力してください")
     private int price;
-    
+
+    @Min(value = 1, message = "セール価格は1以上で入力してください")
     private int salePrice;
+    
+    @Min(value = 0, message = "数量は0以上で入力してください")
     private int stockQuantity;
+
     private String imageUrl;
 
 }
