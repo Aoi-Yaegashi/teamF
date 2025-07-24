4b52d01d68ce7c841ffa3ff00d77d34435d0032b
shogomurata2 <shogo.murata@avantcorp.com>
2025-07-24 17:38:52 +0900

    単体テスト修正

--- raffinehome/src/test/java/com/example/raffinehome/admin/controller/AdminControllerTest2.java
+++ raffinehome/src/test/java/com/example/raffinehome/admin/controller/AdminControllerTest2.java
@@ -437,7 +437,7 @@ class AdminControllerTest2 {
     }
 
 
-        @Test
+    @Test
     @DisplayName("PUT /api/admin/{id} 名前が空の時エラー返却")
     void updateProduct_WhenNameEmpty_ShouldReturnError() throws Exception {
         int id = 5;
@@ -460,6 +460,6 @@ class AdminControllerTest2 {
         mockMvc.perform(put("/api/admin/{id}", id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestJson))
-            .andExpect(status().isInternalServerError());
+            .andExpect(status().isBadRequest());
     }
 }
